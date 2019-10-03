41915b192d0328ca29aae5f95398e5d99ef2ae91
Lcole.engr <lcole.engr@gmail.com>
2019-10-03 18:20:38 -0400

    initial commit framework

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+
+
+
+
+// ***************************************G E O L O C A T I O N ____ F U N C T I O N**************************************************
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
